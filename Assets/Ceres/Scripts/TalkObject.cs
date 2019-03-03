9f222bf0a23d10aed0a35e15647e3fd083102352
Inventrohyder <haitham.hyder@gmail.com>
2019-03-03 04:40:59 -0800

    Initial commit

---
+++ Assets/Ceres/Scripts/TalkObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkObject : MonoBehaviour
+{
+    public void activate(GameObject gameObject)
+    {
+        GameObject[] spinoffs = GameObject.FindGameObjectsWithTag("spinoff");
+        foreach (GameObject spinoff in spinoffs)
+        {
+            Debug.Log(spinoff.name);
+            spinoff.transform.localScale = new Vector3(0, 0, 0);
+        }
+
+        gameObject.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# space-trove
+This is the unity project that Team Eris built for NASA Space Apps Hackathon 2018
+to tackle the virtual space exploration challenge.
