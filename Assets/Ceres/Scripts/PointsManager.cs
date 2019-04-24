2f6a7d3914126a6961e22259fad841dda5e48509
Inventrohyder <haitham.hyder@gmail.com>
2019-04-24 01:25:23 -0700

    Improvements

---
+++ Assets/Ceres/Scripts/PointsManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PointsManager : MonoBehaviour
+{
+    // Access the text game object storing the points of the player
+    public Text pointsText;
+
+    // Store the value of points already earned
+    private int points;
+
+    // Waypoints gameobject
+    public GameObject waypoints;
+
+    // Record the time of the click
+    private float starting_time;
+
+    // Seconds to pause the game
+    private int pausePeriod = 5;
+
+    // Access the Event system
+    GameObject eventSystem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initiate the number of points
+        points = 0;
+        // Move the starting time back so that it doesn't run the first time
+        starting_time = Time.time - pausePeriod;
+
+        // Initialize the event system variable
+        eventSystem = GameObject.Find("GvrEventSystem");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time < starting_time + pausePeriod)
+        {
+            // Remove waypoints
+            waypoints.SetActive(false);
+
+            // Make the counting timer pause
+            eventSystem.GetComponent<TimeKeeper>().wait = false;
+        } else
+        {
+            // Bring back the waypoints
+            waypoints.SetActive(true);
+
+            // Make the counting timer continue
+            eventSystem.GetComponent<TimeKeeper>().wait = true;
+        }
+    }
+
+
+    public void AddPoint(GameObject clickeObject)
+    {
+        // Increase points by 1
+        points++;
+        // Update the shown points
+        pointsText.text = points.ToString();
+
+        // Set starting time to the time of the click
+        starting_time = Time.time;
+
+        // Access this game object's animator and turn it on
+        Animator spinoffAnimator = clickeObject.GetComponent<Animator>();
+        spinoffAnimator.enabled = true;
+        
+    }
+}
---
+++ Assets/Ceres/Scripts/TimeKeeper.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using System;
+
+public class TimeKeeper : MonoBehaviour
+{
+    // The Timer text game object
+    public Text timerText;
+
+
+    // Number of seconds a player can play the game before it ends
+    public float playingTime = 30f;
+
+    // Elapsed time
+    private float time;
+
+    // Difference between last recorded time and now
+    private float nextTime;
+
+    // If wait is on then countdown stops
+    public bool wait;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initiate our current time to 0 at the beginning
+        time = playingTime;
+
+        // Initiate wait to false
+        wait = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Continue counting the time as long as it is less than the maximum
+        // and it is not waiting time
+        if (time > 0 && wait)
+        {
+            // Get the difference between the time in the last frame and now
+            nextTime = Time.deltaTime;
+            // Increase our current time
+            time -= nextTime;
+            // Show our current time rounded off
+            timerText.text = Math.Round(time).ToString();
+        }
+
+
+    }
+
+    void increaseFive()
+    {
+        time += 5;
+    }
+}
