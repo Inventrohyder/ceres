bc5f002aae60b7cd9cd5ef9112035df65e92d990
Inventrohyder <haitham.hyder@gmail.com>
2019-04-25 06:07:17 -0700

    Update project. Leave movement errors

--- Assets/Ceres/Scripts/WaypointMovement.cs
+++ Assets/Ceres/Scripts/WaypointMovement.cs
@@ -2,28 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaypointMovement : MonoBehaviour {
+public class WaypointMovement : MonoBehaviour
+{
 
-	public GameObject player;
+    public GameObject player;
 
-	public bool teleport;
+    public bool teleport;
 
-	private float heightAboveWaypoint = 0.7f;
+    private float heightAboveWaypoint = 0.7f;
 
 
-	//public void Move (GameObject waypoint) {
-	//	if (!teleport) {
-	//		iTween.MoveTo (player,
-	//			iTween.Hash (
-	//				"position", new Vector3 (waypoint.transform.position.x, waypoint.transform.position.y + heightAboveWaypoint, waypoint.transform.position.z),
-	//				"time", .2f,
-	//				"easetype", "linear"
-	//			)
-	//		);
-	//	} else {
-	//		player.transform.position = new Vector3 (waypoint.transform.position.x, waypoint.transform.position.y + heightAboveWaypoint, waypoint.transform.position.z);
-	//	}
-	//}
+    //public void Move (GameObject waypoint) {
+    //  if (!teleport) {
+    //      iTween.MoveTo (player,
+    //          iTween.Hash (
+    //              "position", new Vector3 (waypoint.transform.position.x, waypoint.transform.position.y + heightAboveWaypoint, waypoint.transform.position.z),
+    //              "time", .2f,
+    //              "easetype", "linear"
+    //          )
+    //      );
+    //  } else {
+    //      player.transform.position = new Vector3 (waypoint.transform.position.x, waypoint.transform.position.y + heightAboveWaypoint, waypoint.transform.position.z);
+    //  }
+    //}
 
     // My code 
     public void Move(GameObject waypoint)
